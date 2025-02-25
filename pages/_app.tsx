e838d7307a609d5749c3a001d608d09f5096ee72
Denis Siduna <mnashesduna@gmail.com>
2025-02-25 12:31:57 +0200

    update font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,21 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
-import { Raleway } from 'next/font/google';
+import { Raleway } from "next/font/google";
 
 const raleway = Raleway({
-  subsets: ['latin'],
-  weight: ['100', '300', '400', '500', '700', '900'],
-  style: ['normal', 'italic'],
-  variable: '--font-raleway',
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  style: ["normal", "italic"],
+  variable: "--font-raleway",
 });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className={raleway.variable}>
-    <Header />
-    <Component {...pageProps} />
-    <Footer />
-  </div>;
+  return (
+    <div className={`${raleway.variable} font-sans`}>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
