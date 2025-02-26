369df48006bf5dc09891148c1d0d5cec75ff94c2
Denis Siduna <mnashesduna@gmail.com>
2025-02-26 08:43:45 +0200

    disable unnecessary eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "prefer-const": "off",
     },
   },
 ];
