8839e0bd8a93ae64c1993da50214a834dc52fae2
Denis Siduna <mnashesduna@gmail.com>
2025-03-29 16:21:16 +0200

    update project banner text

--- pages/projects/projects-banner.tsx
+++ pages/projects/projects-banner.tsx
@@ -7,10 +7,10 @@ const ProjectsBanner = ({ selectedCategory }: { selectedCategory: string | null 
     return (
         <Container className='!px-0'>
             <Text variant="title4" color='black'>
-                {`Our ${selectedCategory ? capitalizeFirstCharacter(selectedCategory):''} Projects`}
+                {`Our ${selectedCategory ? capitalizeFirstCharacter(selectedCategory) : ''} Projects`}
             </Text>
             <Text variant="title5" color='brown' additional="w-full max-w-[440px] mt-4 !text-khakhi">
-                Listen to what some of our clients say about our services
+                Ours is a lifelong pledge of consistent quality construction services.
             </Text>
         </Container>
     )
